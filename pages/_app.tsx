63f1aed02a1593d639155d00a46e90ec592b0c7e
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-04-14 19:40:09 +0200

    Add chakra UI library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <ChakraProvider>
             <Head>
                 <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -33,6 +35,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
                 <meta name="theme-color" content="#317EFB" />
             </Head>
             <Component {...pageProps} />
-        </>
+        </ChakraProvider>
     )
 }
